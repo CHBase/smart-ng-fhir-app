3b691790d5aed24d1d6c0e284cb53dbd6e08460e
Ram <g.rmchndrn@gmail.com>
2018-06-28 01:12:34 -0400

    Auto-generated commit

--- main.js
+++ main.js
@@ -2845,18 +2845,6 @@ __webpack_require__.r(__webpack_exports__);
  */
 var CLIENT_APPS = [
     {
-        name: 'CHBase PPE - Sample Standalone and EHR Launch App',
-        uniqueName: 'chbaseppe1',
-        clientId: '6faa0fe6-8eb2-4217-8c4f-0910f7fed0b5',
-        redirectUri: 'http://localhost:4200/redirect/chbaseppe1',
-        launchUrl: 'http://localhost:4200/launch/chbaseppe1',
-        scopes: 'patient/*.*',
-        standalonePatient: true,
-        ehrLaunch: true,
-        server: 'chbaseppe',
-        secret: '5e24b756-9a70-4ea7-a602-150c639280a3'
-    },
-    {
         name: 'CHBase PPE - Sample App - Live Demo',
         uniqueName: 'sampleappdocs',
         clientId: '7be9756f-1cb2-4c7c-9b8c-b2acc2325994',
