ad632ee6c9f67135be4fef63aa9f34c628f247f3
SUMOD U K <sumod@getrealhealth.com>
2019-06-07 18:06:02 +0530

    Added support for attachment download in resource table component.
    Download button is hidden if the resource has no associated document attached.

--- src/app/components/resources/resources-table/resources-table.component.ts
+++ src/app/components/resources/resources-table/resources-table.component.ts
@@ -28,4 +28,32 @@ export class ResourcesTableComponent implements OnInit {
   get codeBundle() {
     return JSON.stringify(this.bundle, null, 2);
   }
+  
+
+  OnDownload(entry){
+    var attachment = entry.resource.content[0].attachment;
+    var url, filename;
+    
+    if(attachment.url){
+        url = attachment.url;
+        filename = attachment.title;
+    }
+    else if(attachment.data)
+    {
+      var data =atob(`${attachment.data}`); 
+      var blob = new Blob([data], { type: `${attachment.contentType}`});
+      url = URL.createObjectURL(blob);
+      filename = entry.resource.id;
+    }
+    
+    if(url){
+      var a = document.createElement('a');
+      a.href = url;
+      a.download = filename;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+    
+  }
 }
